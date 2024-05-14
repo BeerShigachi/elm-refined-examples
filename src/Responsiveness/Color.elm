43a1770af2f4fb36a902438901ae65f0e9896f23
Ayato Shiga-Callaway <beer.shigach@gmail.com>
2024-05-14 14:20:23 +0100

    add Color.elm

---
+++ src/Responsiveness/Color.elm
@@ -0,0 +1,27 @@
+module Responsiveness.Color exposing (..)
+
+import Element exposing (Color, rgb255)
+
+tomato : Color
+tomato =
+    rgb255 255 99 71
+
+
+deepskyblue : Color
+deepskyblue =
+    rgb255 0 191 255
+
+
+gold : Color
+gold =
+    rgb255 255 215 0
+
+
+lightgreen : Color
+lightgreen =
+    rgb255 144 238 144
+
+
+hotpink : Color
+hotpink =
+    rgb255 255 105 180
